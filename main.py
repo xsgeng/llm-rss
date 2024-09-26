2466dd16da9c39ceaa7bc328fb1599746f0e4a32
gengxs <xsgeng@siom.ac.cn>
2024-09-26 16:24:25 +0800

    feat: Add OpenAI integration and duplicate article filtering
    - Integrated OpenAI API for generating replies using the `get_openai_reply` function.
    - Added support for specifying model type ('ollama' or 'openai') in the configuration.
    - Implemented duplicate article filtering to ensure unique titles are processed.
    - Updated the `main` function to handle both Ollama and OpenAI models based on the configuration.
    - Enhanced the `get_ollama_reply` function with additional options for context length and streaming.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from tqdm import tqdm, trange
 import toml
 from queue import Queue
 import threading
+from openai import OpenAI
 
 def prepare_prompt(entry):
     title = entry.title
@@ -62,12 +63,44 @@ def get_ollama_reply(entry, config, model='qwen2.5:32b') -> dict:
             {'role': 'system','content': system_prompt},
             {'role': 'user', 'content': user_prompt}
         ],
-        options=dict(temperature=0, format='json')
+        options=dict(num_ctx=2048, temperature=0, format='json', steam=False)
     )
 
     
     try:
         response_parse = json.loads(response['message']['content'])
+    except json.decoder.JSONDecodeError:
+        response_parse = {"reason": "decode error", "relevance": 0, "impact": 0}
+
+    return response_parse
+
+
+def get_openai_reply(entry, config, model, api_key) -> dict:
+    client = OpenAI(
+        api_key=api_key,
+        base_url="https://api.deepseek.com/beta",
+    )
+
+    system_prompt = prepare_system_prompt(config)
+    
+    user_prompt = prepare_prompt(entry)
+
+    messages = [{"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt}]
+    
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        response_format={
+            'type': 'json_object'
+        },
+        max_tokens=2048,
+        temperature=0,
+        stream=False
+    )
+    
+    try:
+        response_parse = json.loads(response.choices[0].message.content)
     except json.decoder.JSONDecodeError:
         response_parse = {"reason": "decode error", "relevance": 0, "impact": 0}
 
@@ -80,9 +113,16 @@ def main(config_path: Path="config.toml"):
     period = config.get('period', 24)
     relevance_threshold = config.get("relevance_threshold", 5)
     impact_threshold = config.get("impact_threshold", 3)
+
     model = config.get('model', None)
     if model is None:
         raise ValueError("model is not specified in the config file.")
+
+    model_type, model_name = model.split('/')
+    if model_type not in ['ollama', 'openai']:
+        raise ValueError("Invalid model type. Must be 'ollama' or 'openai'.")
+
+    api_key = config.get('api_key', None)
 
 
     new_feed = Rss201rev2Feed(
@@ -102,10 +142,18 @@ def main(config_path: Path="config.toml"):
 
     rss_urls = config['urls']
     recent_entries = []
+    article_titles = []
     for url in rss_urls:
         online_feed = feedparser.parse(url)
 
         recent_entry = list(filter(filter_recent, online_feed.entries))
+
+        # filter duplicated
+        for entry in recent_entry:
+            if entry['title'] not in article_titles:
+                article_titles.append(entry['title'])
+            else:
+                recent_entry.remove(entry)
 
         print(f"{len(recent_entry)} articles  to process on {url}.")
 
@@ -114,7 +162,10 @@ def main(config_path: Path="config.toml"):
 
     q = Queue()
     for entry in recent_entries:
-        t = threading.Thread(target=lambda: q.put(get_ollama_reply(entry, config, model=model)))
+        if model_type == 'ollama':
+            t = threading.Thread(target=lambda: q.put(get_ollama_reply(entry, config, model=model_name)))
+        elif model_type == 'openai':
+            t = threading.Thread(target=lambda: q.put(get_openai_reply(entry, config, model=model_name, api_key=api_key)))
         t.start()
 
     for i in trange(len(recent_entries)):
